4327d7e6e3a602cbb773daf8b73e94933c887605
fr-k9g <67070237@kmitl.ac.th>
2026-02-20 22:58:05 +0700

    add removeDuplicate sequential and parallel

--- src/main/scala/Transform.scala
+++ src/main/scala/Transform.scala
@@ -1,15 +1,31 @@
+import Extract.Anime
+import scala.collection.parallel.CollectionConverters.*
+
 object DemoTransform:
 
-  def process(data: List[Extract.Anime]): List[Extract.Anime] =
+  def process(data: List[Anime]): List[Anime] =
 
-    // 🔹 1. กรอง score > 8.5
-    val filtered = data.filter(_.score > 8.5)
+    // 🔹 0. ลบข้อมูลที่ซ้ำกันก่อนเลย
+    val uniqueData = removeDuplicatesSequential(data)
 
-    // 🔹 2. เรียงคะแนนจากมากไปน้อย
+    // 🔹 1. กรองเอาเฉพาะที่ score > 8.5
+    val filtered = uniqueData.filter(_.score > 8.5)
+
+    // 🔹 2. เรียงคะแนนจากมากไปน้อย (ใส่เครื่องหมายลบหน้า score เพื่อให้เรียงจากมากไปน้อย)
     val sorted = filtered.sortBy(anime => -anime.score)
 
     // 🔹 3. แสดงผล
     println("===== Transform Result =====")
     sorted.foreach(println)
-
+    
     sorted
+
+  // 🔹 1. ลบข้อมูลซ้ำแบบ Sequential (ทำงานทีละแถว)
+  def removeDuplicatesSequential(data: List[Anime]): List[Anime] = {
+    data.distinct
+  }
+
+  // 🔹 2. ลบข้อมูลซ้ำแบบ Parallel (กระจายงานให้ CPU หลาย Core)
+  def removeDuplicatesParallel(data: List[Anime]): List[Anime] = {
+    data.par.distinct.toList
+  }
