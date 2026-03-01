223b501e8a80ae25378c5929a53f5090cbe6436e
GA_IA <flukefoxkung@gmail.com>
2026-03-02 01:30:12 +0700

    Make Main.scala file for running with args

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,16 +7,43 @@ import load.Load._
 import transform.DemoTransform._
 import transform.DateTransform._
 
-def main(args: Array[String]): Unit =
-    val inputPath = "anime.csv"
-    val outputPath = "anime.json"
+def processSequential(inputPath: String, outputPath: String, currentDate: LocalDate): Unit =
+    val data = Extract.extractSequential(inputPath)
+    val ages = mapAge(data, currentDate)
 
-    val currentDate = LocalDate.now()
+    val jsonList = data.zip(ages).map { case (anime, age) =>
+      toJson(anime, age)
+    }
 
+    val qs = quartiles(data)
+
+    val quartileJson =
+      if qs.nonEmpty then
+        s"""|,
+           |   "quartiles": {
+           |      "Q1": ${qs(0)},
+           |      "Q2": ${qs(1)},
+           |      "Q3": ${qs(2)}
+           |   }""".stripMargin
+      else ""
+
+    val finalJson =
+      "{\n" +
+        "  \"data\": [" +
+        jsonList.mkString(",") +
+        "\n  ]" +
+        quartileJson +
+        "\n}"
+
+    saveToJson(finalJson, outputPath)
+    println("Saved to anime.json successfully (Sequential).")
+
+
+def processConcurrent(inputPath : String, outputPath : String, currentDate : LocalDate): Unit =
     val futureData = Extract.extractFuture(inputPath)
 
     val result = futureData.map: list =>
-        val ages = mapAge(list, currentDate)
+        val ages = Await.result(mapAgeFuture(list, currentDate), 5.minutes)
 
         val jsonList =
         list.zip(ages).map { case (anime, age) =>
@@ -47,3 +74,16 @@ def main(args: Array[String]): Unit =
     saveToJson(finalJson, outputPath)
 
     println("Saved to anime.json successfully.")
+
+@main def main(processType: Int): Unit =
+    val inputPath = "anime.csv"
+    val outputPath = "anime.json"
+
+    val currentDate = LocalDate.now()
+
+    val start = System.currentTimeMillis()
+    processType match
+      case 0 => processSequential(inputPath, outputPath, currentDate)
+      case 1 => processConcurrent(inputPath, outputPath, currentDate)
+    println("Running time: " + (System.currentTimeMillis() - start))
+ 
